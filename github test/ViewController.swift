fb38d383e660d2027d73bbd036c46f514ef87273
Brian Seaver <bseaver@d155.org>
2020-04-28 09:53:53 -0500

    initial commit

---
+++ github test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github test
+//
+//  Created by Brian Seaver on 4/28/20.
+//  Copyright © 2020 clc.seaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// first entry
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
